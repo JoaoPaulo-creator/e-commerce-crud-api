3f67a9c15bcd1ab119f1adcaa5196a3c20a77225
JoaoPaulo-creator <joaosarto@gmail.com>
2022-11-01 11:45:35 -0300

    feat/Criar rota de atualizar as informações cadastradas #22 + removido imports inutilizados

--- src/routes.ts
+++ src/routes.ts
@@ -1,12 +1,13 @@
-import { Router, Request, Response } from "express"
+import { Router } from "express"
 import ProductController from "./controllers/ProductController"
 
 
 const router = Router()
 
+router.get('/product', ProductController.getAllProducts)
 router.get('/product/:id', ProductController.getProductById)
 router.post('/product', ProductController.createProduct)
+router.delete('/product/:id', ProductController.deleteProductById)
+router.patch('/product/:id', ProductController.patchProductId)
 
 export default router
-
-
