7c1d52574727883955f9a56b46e6e387b2702a21
JoaoPaulo-creator <joaosarto@gmail.com>
2022-11-01 12:44:00 -0300

    Criar middlware de validação do body#25

--- src/routes.ts
+++ src/routes.ts
@@ -1,13 +1,14 @@
 import { Router } from "express"
 import ProductController from "./controllers/ProductController"
-
+import handleBodyValidation from "./middlewares/handleValidation"
+import bodyRequestValidation from './middlewares/productBodyValidation'
 
 const router = Router()
 
 router.get('/product', ProductController.getAllProducts)
 router.get('/product/:id', ProductController.getProductById)
-router.post('/product', ProductController.createProduct)
+router.post('/product', bodyRequestValidation(), handleBodyValidation, ProductController.createProduct)
 router.delete('/product/:id', ProductController.deleteProductById)
-router.patch('/product/:id', ProductController.patchProductId)
+router.patch('/product/:id', bodyRequestValidation(), handleBodyValidation, ProductController.patchProductId)
 
 export default router
