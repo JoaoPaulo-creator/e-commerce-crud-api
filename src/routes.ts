522b0bb3e39402213480f62c8909934280972249
JoaoPaulo-creator <joaosarto@gmail.com>
2022-11-04 17:15:29 -0300

    .

--- src/routes.ts
+++ src/routes.ts
@@ -1,5 +1,4 @@
 import { Router } from "express"
-import UserRegistrationController from "./controllers/UserRegistrationController"
 import ProductController from "./controllers/ProductController"
 import handleBodyValidation from "./middlewares/handleValidation"
 import bodyRequestValidation from './middlewares/productBodyValidation'
@@ -12,16 +11,7 @@ router.get('/product/:id', ProductController.getProductById)
 
 router.post('/product', bodyRequestValidation(), handleBodyValidation, ProductController.createProduct)
 router.patch('/product/:id', bodyRequestValidation(), handleBodyValidation, ProductController.patchProductId)
-
 router.delete('/product/:id', ProductController.deleteProductById)
-
-
-// rotas de usuários
-router.get('/user/:id', UserRegistrationController.getPersonById)
-router.get('/user', UserRegistrationController.getAllUsers)
-
-router.post('/user_registration', UserRegistrationController.createPerson)
-router.delete('/user/:id', UserRegistrationController.deleteUserById)
 
 
 export default router
