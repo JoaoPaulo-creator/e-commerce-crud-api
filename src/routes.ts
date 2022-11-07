10844c253734c7a0742d73f934c15ddbaa651813
JoaoPaulo-creator <joaosarto@gmail.com>
2022-11-06 22:32:21 -0300

    Removido comentário inútil + ajuste no readme

--- src/routes.ts
+++ src/routes.ts
@@ -5,7 +5,7 @@ import bodyRequestValidation from './middlewares/productBodyValidation'
 
 
 const router = Router()
-// rotas de produtos
+
 router.get('/product', ProductController.getAllProducts)
 router.get('/product/:id', ProductController.getProductById)
 
