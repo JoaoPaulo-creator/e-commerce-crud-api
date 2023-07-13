e72e6ad1fc2cbff7a3415d63b7149b348874b14c
JoaoPaulo-creator <joaosarto@gmail.com>
2023-07-13 18:44:39 -0300

    refactor: Formatado arquivo

--- config/databaseConfig.ts
+++ config/databaseConfig.ts
@@ -1,15 +1,15 @@
-import mongoose from "mongoose"
-import config from "config"
+import mongoose from 'mongoose'
+import config from 'config'
 
 async function dataBaseConnection() {
-    const dbUri = config.get<string>('dbUri')
-    try {
-        await mongoose.connect(dbUri)
-        console.log('✅ Conectado a base de dados com sucesso')
-    } catch (error) {
-        console.error(error)
-        process.exit(1)
-    }
+  const dbUri = config.get<string>('dbUri')
+  try {
+    await mongoose.connect(dbUri)
+    console.log('✅ Conectado a base de dados com sucesso')
+  } catch (error) {
+    console.error(error)
+    process.exit(1)
+  }
 }
 
 export default dataBaseConnection
